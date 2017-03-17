6ff39931335f821b10c3871d684c356ea32f2dbb
Simon Bremer <Simon.Bremer@mercateo.com>
2017-03-17 14:15:08 +0100

    Added preprocessing for LogReg or Ann of User and Item data;

---
+++ content_prep.py
@@ -0,0 +1,53 @@
+import numpy as np
+import pandas as pd
+import pickle
+
+
+def main():
+    ratings_header = ['user_id', 'item_id', 'rating', 'timestamp']
+    ratings = pd.read_csv('data/ml-100k/u.data', sep='\t', names=ratings_header)
+
+    n_ratings = len(ratings)
+    n_users = ratings.user_id.unique().shape[0]
+    n_items = ratings.item_id.unique().shape[0]
+    print('Number of users = ' + str(n_users) + ' | Number of movies = ' + str(n_items))
+
+    # Users from preprocessed user file with locations
+    # ['user_id', 'age', 'sex', 'occupation', 'zip_code', 'loc_lon', 'loc_lat']
+
+    users = pickle.load(open('data/users.pickle', 'rb'))
+
+    n_features_user = 4  # Age, Sex, Location (lon, lat)
+    n_occupations = users.occupation.unique().shape[0]
+    n_features_user +=  n_occupations   # HotOne encoded
+
+    occupation = pd.get_dummies(users.iloc[:, 3])
+
+    # Item Data
+    items_header = ['movie_id', 'title', 'release_date', 'video_release_date', 'imdb_url', 'unknown|0', 'Action|1', 'Adventure|2', 'Animation|3', 'Children\'s|4', 'Comedy|5', 'Crime|6', 'Documentary|7', 'Drama|8', 'Fantasy|9', 'Film-Noir|10', 'Horror|11', 'Musical|12', 'Mystery|13', 'Romance|14', 'Sci-Fi|15', 'Thriller|16', 'War|17', 'Western|18']
+    items = pd.read_csv('data/ml-100k/u.item', sep='|', names=items_header, encoding='latin-1')
+
+    n_features_item = 19  # Genre Tags
+
+    contentX = np.zeros((n_ratings, (n_features_user + n_features_item)))
+    contentY = np.zeros(n_ratings)
+
+    for rating in ratings.itertuples():
+        id = rating.Index
+        userid = rating[1]
+        itemid = rating[2]
+        rating_score = rating[3]
+
+        contentX[id, 0] = users.iloc[userid - 1, 1]  # Age
+        contentX[id, 1] = -1 if users.iloc[userid - 1, 2] == 'M' else 1  # Sex
+        contentX[id, 2:4] = users.iloc[userid - 1, [5, 6]]  # Location (lon, lat)
+        contentX[id, 4:4+n_occupations] = occupation.iloc[userid - 1, :]
+        contentX[id, 25:] = items.iloc[itemid - 1, 5:]
+
+        contentY[id] = rating_score
+
+    pickle.dump((contentX, contentY), open('data/cont.pickle', 'wb'), pickle.HIGHEST_PROTOCOL)
+
+
+if __name__ == '__main__':
+    main()
