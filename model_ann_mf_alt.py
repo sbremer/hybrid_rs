7e1832006ca78e440ed4f85509229c901eba5bda
Simon Bremer <Simon.Bremer@mercateo.com>
2017-04-04 14:32:58 +0200

    Working new MF/ANN model without proper xval yet;

--- model_ann_mf_alt.py
+++ model_ann_mf_alt.py
@@ -9,6 +9,8 @@ import pickle
 from sklearn.metrics import mean_squared_error
 from math import sqrt
 
+import util
+
 # Hardcoded for now
 n_users = 943
 n_items = 1682
@@ -73,34 +75,82 @@ def ann_model(meta_users, meta_items):
     return model
 
 
+def step_mf():
+    Ux, Ix = indexgen.get_indices(int(n_train / 2))
+
+    Yx = model_ann.predict([Ux, Ix]).flatten()
+
+    Ut = np.concatenate((U, Ux))
+    It = np.concatenate((I, Ix))
+    Yt = np.concatenate((Y_scaled, Yx))
+
+    history = model_mf.fit([Ut, It], Yt, batch_size=500, epochs=100, validation_split=0.1, verbose=2, callbacks=callbacks_mf)
+
+
+def test_mf(prnt=False):
+    y_pred = model_mf.predict([U, I]) / 0.2 + 0.5
+    rmse = sqrt(mean_squared_error(Y, y_pred))
+    if prnt:
+        print('RMSE MF: {}'.format(rmse))
+    return rmse
+
+
+def step_ann():
+    Ux, Ix = indexgen.get_indices(int(n_train / 2))
+
+    Yx = model_mf.predict([Ux, Ix]).flatten()
+
+    Ut = np.concatenate((U, Ux))
+    It = np.concatenate((I, Ix))
+    Yt = np.concatenate((Y_scaled, Yx))
+
+    history = model_ann.fit([Ut, It], Yt, batch_size=500, epochs=100, validation_split=0.1, verbose=2, callbacks=callbacks_ann)
+
+
+def test_ann(prnt=False):
+    y_pred = model_ann.predict([U, I]) / 0.2 + 0.5
+    rmse = sqrt(mean_squared_error(Y, y_pred))
+    if prnt:
+        print('RMSE ANN: {}'.format(rmse))
+    return rmse
+
+
 # (meta_users, meta_items, U, I, Y) = pickle.load(open('data/ratings_metadata.pickle', 'wb'))
 (meta_users, meta_items) = pickle.load(open('data/imdb_metadata.pickle', 'rb'))
 (_, U, I, Y) = pickle.load(open('data/cont.pickle', 'rb'))
 
+# Normalize features and set Nans to zero (=mean)
 meta_users = (meta_users - np.nanmean(meta_users, axis=0)) / np.nanstd(meta_users, axis=0)
 meta_items = (meta_items - np.nanmean(meta_items, axis=0)) / np.nanstd(meta_items, axis=0)
 meta_users[np.isnan(meta_users)] = 0
 meta_items[np.isnan(meta_items)] = 0
+
+# Rescale ratings to ~(0.0, 1.0)
 Y_scaled = (Y - 0.5) * 0.2
 
-callbacks = [EarlyStopping('val_loss', patience=4)]
+# Early stopping callbacks. ANN validation varies more -> higher patience
+callbacks_mf = [EarlyStopping('val_loss', patience=4)]
+callbacks_ann = [EarlyStopping('val_loss', patience=10)]
 
 # Create and train MF
-model_mf = mf_model()
-history = model_mf.fit([U, I], Y_scaled, batch_size=500, epochs=100, validation_split=0.1, verbose=2, callbacks=callbacks)
-
-pred = model_mf.predict([U, I]) / 0.2 + 0.5
-
-rmse = sqrt(mean_squared_error(Y, pred))
-print('RMSE MF: {}'.format(rmse))
+model_mf = mf_model(include_bias=True)
+history = model_mf.fit([U, I], Y_scaled, batch_size=500, epochs=100, validation_split=0.1, verbose=2, callbacks=callbacks_mf)
+test_mf(True)
 
 # Create and train ANN
 model_ann = ann_model(meta_users, meta_items)
+history = model_ann.fit([U, I], Y_scaled, batch_size=500, epochs=100, validation_split=0.1, verbose=2, callbacks=callbacks_ann)
+test_ann(True)
 
-history = model_ann.fit([U, I], Y_scaled, batch_size=500, epochs=100, validation_split=0.1, verbose=2, callbacks=callbacks)
+indexgen = util.IndexGen(n_users, n_items, U, I)
+n_train = len(U)
 
-pred = model_ann.predict([U, I]) / 0.2 + 0.5
+for iter in range(3):
+    # MF step
+    step_mf()
+    test_mf(True)
 
-rmse = sqrt(mean_squared_error(Y, pred))
-print('RMSE MF: {}'.format(rmse))
+    # ANN step
+    step_ann()
+    test_ann(True)
 
--- util.py
+++ util.py
@@ -14,3 +14,36 @@ def kfold_entries(k, entries):
         train_indices = [i for i, x in enumerate(entries) if x in train_indices_entries]
         test_indices = [i for i, x in enumerate(entries) if x in test_indices_entries]
         yield train_indices, test_indices
+
+
+class IndexGen:
+    """
+    Generates indices for user-item pairs which are not in the training set.
+    Build for sparse training set! Might be very inefficient else.
+    """
+
+    def __init__(self, n_users, n_items, U, I):
+        assert len(U) == len(I)
+
+        self.n_users = n_users
+        self.n_items = n_items
+
+        # Create and fill entry lookup table
+        self.lookup = {}
+        for u_i in zip(U, I):
+            self.lookup[u_i] = True
+
+    def get_indices(self, n_indices):
+        U = np.zeros((n_indices,))
+        I = np.zeros((n_indices,))
+
+        got = 0
+        while got < n_indices:
+            u = np.random.randint(self.n_users)
+            i = np.random.randint(self.n_items)
+            if (u, i) not in self.lookup:
+                U[got] = u
+                I[got] = i
+                got += 1
+
+        return U, I
