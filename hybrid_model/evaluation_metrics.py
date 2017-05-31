880a52f99375e8a2795e1730b783f686649552f7
Simon Bremer <Simon.Bremer@mercateo.com>
2017-05-31 15:11:40 +0200

    Added mean precision at measure;

--- hybrid_model/evaluation_metrics.py
+++ hybrid_model/evaluation_metrics.py
@@ -20,6 +20,40 @@ class Mae(Metric):
         return mae
 
 
+class Precision(Metric):
+    def __init__(self, k):
+        self.k = k
+
+    def calculate(self, y_true, y_pred, x):
+        inds_u = x[0]
+        users = np.unique(inds_u)
+
+        precisions = np.zeros((len(users),))
+
+        for i, user in enumerate(users):
+            user_i = (inds_u == user).flatten()
+
+            y_true_u = y_true[user_i]
+            y_pred_u = y_pred[user_i]
+
+            sort_true = np.argsort(-y_true_u)
+            sort_pred = np.argsort(-y_pred_u)
+
+            pred = list(sort_pred[:self.k])
+            true = list(sort_true[:self.k])
+
+            for r in sort_true[self.k:]:
+                if y_true_u[r] == y_true_u[true[-1]]:
+                    true.append(y_true_u[r])
+                else:
+                    break
+
+            precision_u = sum(1 for p in pred if p in true) / self.k
+            precisions[i] = precision_u
+
+        return np.mean(precisions)
+
+
 class Ndcg(Metric):
     def __init__(self, k):
         self.k = k
